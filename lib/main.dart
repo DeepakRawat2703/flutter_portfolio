c10c0200f6568b41b43b727e252e8c4a123474f3
DeepakRawat2703 <85488979+DeepakRawat2703@users.noreply.github.com>
2021-10-04 17:36:02 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutterportfolio/landing_page.dart';
+
+void main() {
+  runApp(const PortfolioApp());
+}
+
+class PortfolioApp extends StatelessWidget {
+  const PortfolioApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
